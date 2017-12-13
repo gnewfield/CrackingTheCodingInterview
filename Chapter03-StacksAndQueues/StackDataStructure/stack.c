622083847e563cdcf6de5c445a87acef21ebd726
graynewfield <graynewfield@gmail.com>
2017-12-13 15:06:44 -0500

    Built stack data structure

---
+++ Chapter03-StacksAndQueues/StackDataStructure/stack.c
@@ -0,0 +1,5 @@
+// C implementation of a stack data structure
+////////////////////////////////////////////////////////////////////////////////
+#include "../../Chapter02-LinkedLists/LinkedListDataStructure/linkedList.h"
+
+typedef Item Node;
